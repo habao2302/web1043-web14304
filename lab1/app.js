7b3b3996f38c8c0a1e3b9e05474eead86412af1c
ha bao <baohdpd08126@fpt.edu.vn>
2023-03-10 00:01:32 +0700

    nop lab1

---
+++ lab1/app.js
@@ -0,0 +1,9 @@
+// nhap gia tri a
+let a = prompt("nhap so tu ban phim");
+// nhap gia tri b
+
+let b = prompt("nhap so tu ban phim");
+// tinh tong a b
+let c= parseInt(a)+parseInt(b);
+document.write(c);
+console.log(c);
